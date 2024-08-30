3c7da88a511a22d6eca05560f7297414cf45928f
haadiyaa <haadiyact@gmail.com>
2024-08-30 12:25:38 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/machinetest_kalbas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.machinetest_kalbas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/bloc/bloc/news_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'news_event.dart';
+part 'news_state.dart';
+
+class NewsBloc extends Bloc<NewsEvent, NewsState> {
+  NewsBloc() : super(NewsInitial()) {
+    on<NewsEvent>((event, emit) {
+    });
+  }
+}
